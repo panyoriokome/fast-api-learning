908db8a712aaa474dade527dc56572ae077b6c7a
panyoriokome <panyoriokome@panyoriokomenoiMac.local>
2021-11-28 08:35:56 +0900

    tutorial: Request Body

--- tutorial/tutorial/main.py
+++ tutorial/tutorial/main.py
@@ -1,11 +1,22 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
-    lenet = "lenet"   
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -13,6 +24,7 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
 
 @app.get("/users/me")
 async def read_user_me():
@@ -23,6 +35,7 @@ async def read_user_me():
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
+
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
@@ -32,3 +45,25 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item_with_path(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+
+@app.put("/items/{item_id}")
+async def create_item_with_path_and_query(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
